7918309e3820b6a64b7275784be071769e09a21f
Coltisor Victor <coltisor.victor@gmail.com>
2022-01-20 01:01:55 +0200

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -24,7 +24,7 @@ window.onbeforeunload = function () {
 async function registerSW() {
     if ('serviceWorker' in navigator) {
         try {
-            await navigator.serviceWorker.register('sw.js');
+            await navigator.serviceWorker.register('./sw.js');
         } catch (e) {
             console.log("SW Registration error:", e);
         }
